a15fc5a78327f2ef6b29f8b766a359b2c5b9c168
Angel Baro <angelbaro44@gmail.com>
2021-01-19 18:40:20 -0500

    Docs added to solution

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//I will make an object and have the array values as the object key
+//and the times repeated as the key value.
+// EX: [1,1,2,2,2,3,4,4,5] | object:{ '1':2, '2':3, '3':1, '4':2, '5':1}
+//I then return any object key less then or equal to the number given 'n'
+
+
 function deleteNth(arr, n) {
       var valueStorageList = {};
       return arr.filter(x => {
